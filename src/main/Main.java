300cb3bd4518bacc16dbbcca7daee1d67443fc46
eltoon081190 <56300463+eltoon081190@users.noreply.github.com>
2020-05-10 22:06:47 +0100

    a few strutural modifications

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import homepage.HomepageController;
+
+/**
+ * this is the Ultra-Vision system
+ * developed through the 4th semester of 
+ * Bsc in IT at CCT Colleg Duublin
+ * @author elton silva id 2018322
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new HomepageController();
+
+	}
+
+}
